ffb6e05c3b9aeeec75afe4924012c9c96a3ef121
Jaypal-Singh <jaypalsinghchouhan2008@gmail.com>
2025-06-14 19:01:09 +0530

    jo kaam kiya uska message

---
+++ backend/src/Model/FormModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { nanoid } = require("nanoid");
+
+const formSchema = new mongoose.Schema(
+  {
+    // generate a ~10-character unique ID on each new doc:
+    uniquckId: {
+      type: String,
+      default: () => nanoid(10),
+    },
+    clientName: { type: String },
+    stockName: { type: String },
+    idCode: { type: String },
+    quantity: { type: Number },
+    buyPrice: { type: Number },
+    sellPrice: { type: Number },
+    tradeDate: { type: Date },
+    brokerage: { type: Number },
+    address: { type: String },
+    margin: { type: Number },
+    mobileNumber: { type: Number },
+    mode: {
+      type: String,
+      enum: ["buy", "sell"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const FormModel = mongoose.model("FormModel", formSchema);
+module.exports = { FormModel };
---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const mongoose = require("mongoose");
+const cors = require("cors");
+app.use(express.json());
+const MONGO_URL = process.env.MONGO_URL;
+const formRoute = require("./Routes/FormRoute");
+
+app.use(cors({
+    origin : [
+        "http://localhost:3000"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}))
+
+app.use('/api/forms', formRoute);
+
+mongoose.connect(MONGO_URL)
+  .then(() => {
+    console.log("mongoDB is Connect");
+    app.listen(PORT, () => {
+      console.log(`Server is Listen on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
